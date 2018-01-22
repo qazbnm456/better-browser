eb205ed005a260bb39ab67d51600e2b0ea935f51
Boik <qazbnm456@gmail.com>
2018-01-22 16:41:58 +0800

    basic structure

---
+++ build/webpack.config.js
@@ -0,0 +1,89 @@
+'use strict'
+
+process.env.BABEL_ENV = 'main'
+
+const path = require('path')
+const { dependencies } = require('../package.json')
+const webpack = require('webpack')
+
+const MinifyPlugin = require('babel-minify-webpack-plugin')
+
+let config = {
+  entry: {
+    index: path.join(__dirname, '../src/index.ts')
+  },
+  externals: [
+    ...Object.keys(dependencies || {})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: {
+          loader: 'tslint-loader',
+          options: {
+            typeCheck: true
+          }
+        }
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+            onlyCompileBundledFiles: true
+          }
+        }
+      },
+      {
+        test: /\.(js)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '../dist')
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  target: 'node'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new MinifyPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })
+  )
+}
+
+module.exports = config
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function test() {
+  // tslint:disable-next-line no-console
+  console.log(1);
+}
+
+export default test;
